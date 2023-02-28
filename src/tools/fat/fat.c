c3b7b0e39a089179e85d2a49a3f8134488414161
nizomovasadbek <nizomovasadbekjava@gmail.com>
2023-02-28 20:37:23 +0500

    Fat12 desinged

--- src/tools/fat/fat.c
+++ src/tools/fat/fat.c
@@ -1,4 +1,92 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+
+typedef struct {
+
+    uint8_t BootJumpInstruction[3];
+    uint8_t OemIdentifier[8];
+    uint16_t BytesPerSector;
+    uint8_t SectorsPerCluster;
+    uint16_t ReservedSectors;
+    uint8_t FatCount;
+    uint16_t DirEntryCount;
+    uint16_t TotalSectors;
+    uint8_t MediaDescriptorType;
+    uint16_t SectorsPerFat;
+    uint16_t SectorsPerTrack;
+    uint16_t Heads;
+    uint32_t HiddenSectors;
+    uint32_t LargeSectorCount;
+
+    uint8_t DriveNumber;
+    uint8_t _Reserved;
+    uint8_t Signature;
+    uint32_t VolumeId;
+    uint8_t VolumeLabel[11];
+    uint8_t SystemId[8];
+
+} __attribute__((packed)) BootSector;
+
+typedef struct {
+    uint8_t Name[11];
+    uint8_t Attributes;
+    uint8_t _Reserved;
+    uint8_t CreateTimeTenths;
+    uint16_t CreatedTime;
+    uint16_t CreatedDate;
+    uint16_t AccessedDate;
+    uint16_t FirstClusterHigh;
+    uint16_t ModifyTime;
+    uint16_t ModifyDate;
+    uint16_t FirstClusterLow;
+    uint32_t Size;
+
+} __attribute__((packed)) DirectoryEntry;
+
+BootSector g_BootSector;
+uint8_t* g_Fat = NULL;
+DirectoryEntry* g_RootDirectory = NULL;
+
+bool readBootSector(FILE* disk){
+    return fread(&g_BootSector, sizeof(g_BootSector), 1, disk) > 0;
+}
+
+bool readSectors(FILE* disk, uint32_t lba, uint32_t count, void* bufferOut){
+    bool ok = true;
+    ok = ok && (fseek(disk, lba * g_BootSector.BytesPerSector, SEEK_SET) == 0);
+    ok = ok && (fread(bufferOut, g_BootSector.BytesPerSector, count, disk)== count);
+    return ok;
+}
+
+bool readFat(FILE* disk){
+    g_Fat = (uint8_t*) malloc(g_BootSector.SectorsPerFat * g_BootSector.BytesPerSector);
+    return readSectors(disk, g_BootSector.ReservedSectors, g_BootSector.SectorsPerFat, g_Fat);
+}
+
+bool readRootDirectory(FILE* disk){
+    uint32_t lba = g_BootSector.ReservedSectors + g_BootSector.SectorsPerFat * g_BootSector.FatCount;
+    uint32_t size = sizeof(DirectoryEntry) * g_BootSector.DirEntryCount;
+    uint32_t sectors = (size / g_BootSector.BytesPerSector);
+    if(size % g_BootSector.BytesPerSector > 0){
+        sectors++;
+    }
+
+    g_RootDirectory = (DirectoryEntry*) malloc(sectors * g_BootSector.BytesPerSector);
+    return readSectors(disk, lba, sectors, g_RootDirectory);
+}
+
+DirectoryEntry* findFile(const char* name){
+    for(uint32_t i = 0; i < g_BootSector.DirEntryCount; i++){
+        if(memcmp(name, g_RootDirectory[i].Name, 11) == 0){
+            return &g_RootDirectory[i];
+        }
+    }
+
+    return NULL;
+}
 
 int main(int argc, char** argv){
     if(argc < 3){
@@ -6,5 +94,40 @@ int main(int argc, char** argv){
         return -1;
     }
 
+    FILE* disk = fopen(argv[1], "rb");
+
+    if(!disk){
+        fprintf(stderr, "Disk faylni ochib bo'lmadi %s\n", argv[1]);
+        return -1;
+    }
+    
+    if(!readBootSector(disk)){
+        fprintf(stderr, "Boot sektorini o'qib bo'lmadi\n");
+        return -2;
+    }
+
+    if(!readFat(disk)){
+        fprintf(stderr, "FAT formatida diskni o'qib bo'lmadi\n");
+        free(g_Fat);
+        return -3;
+    }
+
+    if(!readRootDirectory(disk)){
+        fprintf(stderr, "Asosiy papkani o'qib bo'lmadi\n");
+        free(g_Fat);
+        free(g_RootDirectory);
+        return -4;
+    }
+
+    DirectoryEntry* fileEntry = findFile(argv[2]);
+    if(!fileEntry){
+        fprintf(stderr, "Fayl topilmadi %s\n", argv[2]);
+        free(g_Fat);
+        free(g_RootDirectory);
+        return -5;
+    }
+
+    free(g_RootDirectory);
+    free(g_Fat);
     return 0;
 }
