35bb9aad541dbc76966fe41a95dfdcf1a981a0cb
nizomovasadbek <nizomovasadbekjava@gmail.com>
2023-03-10 17:19:42 +0500

    Printf implement

--- src/bootloader/stage2/stdio.c
+++ src/bootloader/stage2/stdio.c
@@ -11,3 +11,52 @@ void puts(const char* str){
         *str++;
     }
 }
+
+void _cdecl printf(const char *fmt, ...){
+    int* argp = (int*) &fmt;
+    int state = PRINTF_STATE_NORMAL;
+    int length = PRINTF_LENGTH_DEFAULT;
+
+    argp++;
+
+    while(*fmt) {
+
+        switch(state) {
+            case PRINTF_STATE_NORMAL:
+                switch(*fmt) {
+                    case '%':
+                        state = PRINTF_STATE_LENGTH;
+                        break;
+                    default:
+                        putc(*fmt);
+                        break;
+                }
+                break;
+            case PRINTF_STATE_LENGTH:
+                switch(*fmt) {
+                    case 'h':
+                        length = PRINTF_LENGTH_SHORT;
+                        state = PRINTF_STATE_LENGTH_SHORT;
+                        break;
+                    case 'l':
+                        length = PRINTF_LENGTH_LONG;
+                        state = PRINTF_STATE_LENGTH_LONG;
+                        break;
+
+                    default:
+                        goto PRINTF_STATE_SPEC_;
+                }
+                break;
+            case PRINTF_STATE_LENGTH_SHORT:
+                if(*fmp == 'h'){
+                    length = PRINTF_LENGTH_SHORT_SHORT;
+                    state = PRINTF_STATE_SPEC;
+                } else goto PRINTF_STATE_SPEC_;
+                break;
+
+        }
+
+        fmt++;
+    }
+    
+}
