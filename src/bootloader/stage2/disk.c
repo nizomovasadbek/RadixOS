9787e1fb35cb93f161b064a05f418aa7620d83d1
nizomovasadbek <nizomovasadbekjava@gmail.com>
2023-05-17 17:29:13 +0500

    Bug fixed #0001

--- src/bootloader/stage2/disk.c
+++ src/bootloader/stage2/disk.c
@@ -1,6 +1,21 @@
 #include "disk.h"
 #include "x86.h"
 
+bool DISK_Initialize(DISK* disk, uint8_t driveNumber) {
+    uint8_t driveType;
+    uint16_t cylinders, sectors, heads;
+
+    if(!x86_Disk_GetDriveParams(disk->id, &driveType, &cylinders, &sectors, &heads)) {
+        return false;
+    }
+    disk->id = driveNumber;
+    disk->cylinders = cylinders;
+    disk->heads = heads;
+    disk->sectors = sectors;
+
+    return true;
+}
+
 void DISK_LBA2CHS(DISK* disk, uint32_t lba, uint16_t* cylindersOut, uint16_t* sectorsOut, uint16_t* headsOut) {
     *sectorsOut = lba % disk->sectors + 1;
     *cylindersOut = (lba / disk->sectors) / disk->heads;
